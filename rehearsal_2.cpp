a7b4a6b2004c5d3fac9e5e1a1ad3f333f4abeacf
konglarock <59278025+konglarock@users.noreply.github.com>
2020-01-10 17:32:09 +0700

    Update rehearsal_2.cpp

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    float x,sum=0;
+
+    cout<<"Enter x: ";
+    cin>>x;
+    
+    while (x!=0)
+    {
+        sum=sum+x;
+        cout<<"Enter x: ";
+        cin>>x;
+        
+    }
+
+    if (x==0)
+    {
+        sum=sum+x;
+        cout<<"sum = "<<sum;
+    }
+
+    
+    
+    
+    return 0;
+
+}
