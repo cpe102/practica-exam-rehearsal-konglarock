880d99f353e9f7175a302ec9342fe939975ee899
konglarock <59278025+konglarock@users.noreply.github.com>
2020-01-10 17:30:59 +0700

    Update rehearsal_1.cpp

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    float G=3.50, s=0;
+    string Name;
+
+    cout<<"What is your name?: ";
+    cin>>Name;
+    cout<<"whait is your GPA?: ";
+    cin>>s;
+    if (s>=G)
+    {
+        cout<<Name<<" Inw jrim jrim!!!";
+    }
+    else
+    {
+        cout<<"Try harder, "<< Name;
+    }
+    
+    return 0;
+
+}
